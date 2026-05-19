395dec86d1c7e9cbc016881f0b574b4b68138f5a
Mounika <dsmounikareddy15@gmail.com>
2026-05-19 18:48:57 +0530

    testing automation trigger

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,10 +9,9 @@ export default defineConfig({
   testDir: './tests',
   // fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  // retries: process.env.CI ? 2 : 0,
-  // retries:1,
-  workers : 1,
-  // workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 2 : 0,
+  // workers : 1,
+  workers: process.env.CI ? 1 : undefined,
   reporter: [['html',{open:'never'}],['json', { outputFile: 'results.json' }]
 ],
 
